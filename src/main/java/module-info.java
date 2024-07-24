28dc92739a1517e376ccc2c37605e478cdd47ea3
Surfingchicken <alex.delf@laposte.net>
2024-07-24 16:41:22 +0200

    end

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,12 @@ module com.example.demo {
     requires com.fasterxml.jackson.databind;
     requires jakarta.persistence;
     requires com.calendarfx.view;
+
     opens com.example.demo to javafx.fxml;
     opens back.java.core.services to com.fasterxml.jackson.databind;
     opens back.java.core.dto to javafx.base;
+
     exports com.example.demo;
     exports back.java.core.dto to com.fasterxml.jackson.databind;
     exports back.java.core.datas to com.fasterxml.jackson.databind;
-
 }
